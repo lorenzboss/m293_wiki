93fe9f7b6ca0f754bb26489e4bf88c30d8e637db
lorenzboss <lorenz.boss@bbzbl-it.ch>
2024-04-14 13:25:47 +0200

    new routing

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   var currentYear = new Date().getFullYear();
 
-  document.querySelector('nav').innerHTML = `
+  document.querySelector("nav").innerHTML = `
       <h2><a href="0_homepage.html">Übersicht</a></h2>
       <ul>
-          <li><a href="1_harry_potter.html">Harry Potter</a></li>
-          <li><a href="2_hermine_granger.html">Hermine Granger</a></li>
-          <li><a href="3_ron_weasley.html">Ron Weasley</a></li>
-          <li><a href="4_draco_malfoy.html">Draco Malfoy</a></li>
-          <li><a href="5_albus_dumbledore.html">Albus Dumbledore</a></li>
-          <li><a href="6_severus_snape.html">Severus Snape</a></li>
-          <li><a href="a_podcast_empfehlung.html">Podcast Empfehlung</a></li>
+          <li><a href="../harry">Harry Potter</a></li>
+          <li><a href="../hermine">Hermine Granger</a></li>
+          <li><a href="../ron">Ron Weasley</a></li>
+          <li><a href="../draco">Draco Malfoy</a></li>
+          <li><a href="../dumbledore">Albus Dumbledore</a></li>
+          <li><a href="../snape">Severus Snape</a></li>
+          <li><a href="../podcast">Podcast Empfehlung</a></li>
       </ul>
   `;
 
-  document.querySelector('footer').innerHTML = `    
+  document.querySelector("footer").innerHTML = `    
       <div>
           <section class="section">
               <h3>FORMALES</h3>
-              <p><a href="b_impressum.html">Impressum</a></p>
+              <p><a href="../impressum">Impressum</a></p>
           </section>
 
           <section class="section">
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
           <section class="section">
               <h3>KOMMENTAR</h3>
-              <p>Hier können Sie einen <a href="c_kommentar.html">Kommentar über die Webseite</a> abgeben</p>
+              <p>Hier können Sie einen <a href="../comments">Kommentar über die Webseite</a> abgeben</p>
           </section>
       </div>
       <p id="rechte">Alle Rechte vorbehalten &copy;-${currentYear} Lorenz Boss</p>
